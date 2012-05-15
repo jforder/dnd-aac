003324523e2727533408e2fed933a4646e7a7a41
davidly16@gmail.com <davidly16@gmail.com@2b09df3e-bf32-aa2d-f863-fad657680e6d>
2012-05-15 13:25:26 +0000

    Initial import.

---
+++ trunk/dnd-aac/src/com/dnd/aac/DetailFragment.java
@@ -0,0 +1,35 @@
+package com.dnd.aac;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class DetailFragment extends android.support.v4.app.Fragment {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Log.e("Test", "hello");
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.details, container, false);
+		return view;
+	}
+
+	public void setText(String item) {
+		TextView view = (TextView) getView().findViewById(R.id.detailsText);
+		view.setText(item);
+	}
+}
