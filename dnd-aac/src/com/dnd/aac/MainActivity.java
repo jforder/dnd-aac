a447a07fac4fcde4bd9da8049cb4e7b4aad4980a
dlieu.88 <Dlieu.88@gmail.com>
2012-05-16 22:21:53 +0000

    Edit Textbox now has a button to delete last word on single tap; all words on double tap

--- dnd-aac/src/com/dnd/aac/MainActivity.java
+++ dnd-aac/src/com/dnd/aac/MainActivity.java
@@ -4,22 +4,33 @@ import java.util.Locale;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTransaction;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
+import android.view.MotionEvent;
 
 public class MainActivity extends FragmentActivity implements OnItemClickListener, OnInitListener {
 	private int MY_DATA_CHECK_CODE = 0;
 	private TextToSpeech myTTS;
+	
+	private GestureDetector mGestureDetector;
     
 	/** Called when the activity is first created. */
     @Override
@@ -31,7 +42,10 @@ public class MainActivity extends FragmentActivity implements OnItemClickListene
         checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
         startActivityForResult(checkIntent, MY_DATA_CHECK_CODE);
         
-    }    
+        initEditText(); //attach listeners etc     
+    }   
+
+    
     public void saySomething(View view)  
     {  
     	 EditText enteredText = (EditText)findViewById(R.id.enter);
@@ -44,8 +58,6 @@ public class MainActivity extends FragmentActivity implements OnItemClickListene
             if (resultCode == TextToSpeech.Engine.CHECK_VOICE_DATA_PASS) {
                 // success, create the TTS instance
             	myTTS = new TextToSpeech(this, this);
-            	
-
             } else {
                 // missing data, install it
                 Intent installIntent = new Intent();
@@ -53,11 +65,7 @@ public class MainActivity extends FragmentActivity implements OnItemClickListene
                     TextToSpeech.Engine.ACTION_INSTALL_TTS_DATA);
                 startActivity(installIntent);
             }
-            
-            
-        }
-
-        
+        }    
     }
 
 	@Override
@@ -76,6 +84,68 @@ public class MainActivity extends FragmentActivity implements OnItemClickListene
 		//
 	}
 	
+	private void initEditText()
+	{
+		final EditText enteredText = (EditText)findViewById(R.id.enter);
+		enteredText.setFocusable(false); //Cannot click in textbox
+
+		final Drawable x = getResources().getDrawable(android.R.drawable.presence_offline);//your x image, this one from standard android images looks pretty good actually
+		x.setBounds(0, 0, x.getIntrinsicWidth(), x.getIntrinsicHeight());
+		enteredText.setCompoundDrawables(null, null, null, null);
+
+		mGestureDetector = new GestureDetector(new SimpleOnGestureListener()
+		{
+			@Override
+			public boolean onDoubleTap(MotionEvent e) {
+				enteredText.setText("");
+				enteredText.setCompoundDrawables(null, null, null, null);
+				return false;
+			}
+
+			@Override
+			public boolean onSingleTapConfirmed(MotionEvent e) {         	
+				String txt = enteredText.getText().toString();   
+				//Replace last alphanumeric word regardless of leading or trailing white space
+				txt = txt.replaceFirst("\\s*\\w+\\s*$", "");
+				if(TextUtils.isEmpty(txt)) enteredText.setCompoundDrawables(null, null, null, null);;
+				enteredText.setText(txt);
+				return false;
+			}
+		});
+
+		enteredText.setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+
+				if (enteredText.getCompoundDrawables()[2] == null) {
+					return false;
+				}
+				if (event.getAction() != MotionEvent.ACTION_DOWN 
+						&& event.getAction() !=  MotionEvent.ACTION_UP) {
+					return false;
+				}
+
+				if (event.getX() > enteredText.getWidth() - enteredText.getPaddingRight() - x.getIntrinsicWidth()) { 
+					mGestureDetector.onTouchEvent(event);     
+				}
+				return false;
+			}
+		});
+		enteredText.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				enteredText.setCompoundDrawables(null, null, enteredText.getText().toString().equals("") ? null : x, null);
+			}
+
+			@Override
+			public void afterTextChanged(Editable arg0) {
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+			}
+		});
+	}
 	/*public void onDestroy(){
 		myTTS.shutdown();		
 	}*/
