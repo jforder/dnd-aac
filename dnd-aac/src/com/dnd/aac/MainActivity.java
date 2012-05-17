6ee2c29ff2654577268c04c1912a367db46dfe72
davidly16 <davidly16@gmail.com>
2012-05-17 19:08:19 +0000

    Header on top of list implemented

--- dnd-aac/src/com/dnd/aac/MainActivity.java
+++ dnd-aac/src/com/dnd/aac/MainActivity.java
@@ -1,32 +1,38 @@
 package com.dnd.aac;
 
+import java.util.HashMap;
 import java.util.Locale;
 
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
+import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
 import android.support.v4.app.FragmentActivity;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.View;
 import android.view.View.OnTouchListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.view.MotionEvent;
 
-public class MainActivity extends FragmentActivity implements OnItemClickListener, OnInitListener {
+public class MainActivity extends FragmentActivity implements OnItemClickListener, OnInitListener, TextToSpeech.OnUtteranceCompletedListener {
 	private int MY_DATA_CHECK_CODE = 0;
 	private TextToSpeech myTTS;
-	
+	private HashMap<String, String> myHashAlarm = new HashMap();
 	private GestureDetector mGestureDetector;
-    
+	private Handler mHandler; 
+	
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,15 +43,32 @@ public class MainActivity extends FragmentActivity implements OnItemClickListene
         checkIntent.setAction(TextToSpeech.Engine.ACTION_CHECK_TTS_DATA);
         startActivityForResult(checkIntent, MY_DATA_CHECK_CODE);
         
-        initEditText(); //attach listeners etc     
+        initEditText(); //attach listeners etc  
+        
+        mHandler = new Handler();
+        
+        myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_STREAM,
+                String.valueOf(AudioManager.STREAM_ALARM));
+        myHashAlarm.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "test");
     }   
 
     
     public void saySomething(View view)  
-    {  
+    {
+    	Button speakBtn =(Button) findViewById(R.id.button1);
+    	if (!myTTS.isSpeaking()){
     	 EditText enteredText = (EditText)findViewById(R.id.enter);
          String words = enteredText.getText().toString();
-         myTTS.speak(words, TextToSpeech.QUEUE_FLUSH, null);
+	    	speakBtn.setText(getString(R.string.speakbtncancel));
+         myTTS.speak(words, TextToSpeech.QUEUE_FLUSH, myHashAlarm);
+    	} 
+    	else 
+    	{
+    		speakBtn.setText(getString(R.string.speakbtn));
+    		myTTS.stop();
+    	}
+
+         
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -67,11 +90,25 @@ public class MainActivity extends FragmentActivity implements OnItemClickListene
 	public void onInit(int initStatus) {
 	    if (initStatus == TextToSpeech.SUCCESS) {
 	        myTTS.setLanguage(Locale.US);
+	        myTTS.setOnUtteranceCompletedListener(this);
 	    }
 	    else if (initStatus == TextToSpeech.ERROR) {
 	        Toast.makeText(this, "Sorry! Text To Speech failed...", Toast.LENGTH_LONG).show();
 	    }
 
+	}
+	
+	public void onUtteranceCompleted(String uttId) {
+		if (uttId.equalsIgnoreCase("test")){			
+			// Create runnable for posting
+		    Runnable mUpdateResults = new Runnable() {
+		        public void run() {
+		        	Button speakBtn =(Button) findViewById(R.id.button1);
+			    	speakBtn.setText(getApplicationContext().getString(R.string.speakbtn));
+		        }
+		    };
+		    mHandler.post(mUpdateResults);
+		}
 	}
 
 	@Override
