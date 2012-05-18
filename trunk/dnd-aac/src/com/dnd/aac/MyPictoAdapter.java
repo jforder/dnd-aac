8388bd4cfba992b286a8bbde4be144058341d85b
Dlieu.88@gmail.com <Dlieu.88@gmail.com@2b09df3e-bf32-aa2d-f863-fad657680e6d>
2012-05-18 14:52:14 +0000

    Picto layout updated

--- trunk/dnd-aac/src/com/dnd/aac/MyPictoAdapter.java
+++ trunk/dnd-aac/src/com/dnd/aac/MyPictoAdapter.java
@@ -2,24 +2,27 @@ package com.dnd.aac;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
-
 import android.content.Context;
 import android.content.res.AssetManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class ImageAdapter extends BaseAdapter{
+public class MyPictoAdapter extends BaseAdapter{
 	  private Context mContext;
+	  private int layout;
 	  private Cursor mCursor;
-
-	    public ImageAdapter(Context c,Cursor cursor) {
+	  
+	    public MyPictoAdapter(Context c,int layout,Cursor cursor) {
 	        mContext = c;
+	        this.layout = layout;
 	        mCursor = cursor;
 	    }
 
@@ -33,21 +36,19 @@ public class ImageAdapter extends BaseAdapter{
 
 	    public long getItemId(int position) {
 	        mCursor.moveToPosition(position);
-	        return mCursor.getLong(0);
+	        return mCursor.getLong(mCursor.getColumnIndexOrThrow("_id"));
 	    }
 
 	    // create a new ImageView for each item referenced by the Adapter
 	    public View getView(int position, View convertView, ViewGroup parent) {
-	        ImageView imageView;
+	        View picto;
 	        if (convertView == null) {  // if it's not recycled, initialize some attributes
 	        	mCursor.moveToPosition(position);
 	        	
-	            imageView = new CustomImageView(mContext,mCursor.getString(mCursor.getColumnIndex("itemPhrase")));
-	            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
-	            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-	            imageView.setPadding(8, 8, 8, 8);
-	            
-	            
+	        	picto = ( (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) ).inflate(layout,null);	        	
+	        	picto.setLayoutParams(new GridView.LayoutParams(115,115));	
+	        	
+	        	ImageView imageView = (ImageView)picto.findViewById(R.id.image);
 	            AssetManager am = mContext.getAssets();
 		        try {
 					BufferedInputStream buf = new BufferedInputStream(am.open("images/" + mCursor.getString(mCursor.getColumnIndex("imageUri"))));
@@ -60,13 +61,15 @@ public class ImageAdapter extends BaseAdapter{
 					e.printStackTrace();
 				}
 		        
+		        TextView tv = (TextView)picto.findViewById(R.id.text);
+		        tv.setText(mCursor.getString(mCursor.getColumnIndex("itemPhrase")));
+		             
 	        } else {
-	            imageView = (CustomImageView) convertView;
+	        	picto = convertView;	        	
 	        }	   
-
-	        
-	       
-	        return imageView;
+    
+	        return picto;
 	    }
 }
+
 
