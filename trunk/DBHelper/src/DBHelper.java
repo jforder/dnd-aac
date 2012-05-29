347dbac43260042ff27e3cd637bc6b78c015690f
Dlieu.88@gmail.com <Dlieu.88@gmail.com@2b09df3e-bf32-aa2d-f863-fad657680e6d>
2012-05-29 15:28:04 +0000

    Initial Commit. Project to make database for AAC app

---
+++ trunk/DBHelper/src/DBHelper.java
@@ -0,0 +1,186 @@
+import java.io.File;
+import java.sql.*;
+import java.util.HashMap;
+public class DBHelper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PreparedStatement prep;
+		ResultSet keys;
+		HashMap<String,Long> catMap = new HashMap<String,Long>(100);
+		HashMap<String,Long> subMap = new HashMap<String,Long>(150);
+		
+		Connection conn = null;
+		try{
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection("jdbc:sqlite:aac_data");
+			Statement stat = conn.createStatement();
+			
+			stat.executeUpdate("drop table if exists Categorys;");
+			stat.executeUpdate("drop table if exists Subcategorys;");
+			stat.executeUpdate("drop table if exists Pictos;");
+			stat.executeUpdate("drop table if exists RecentPictos;");
+			stat.executeUpdate("drop table if exists Images;");
+			stat.executeUpdate("drop table if exists Subcategorys_Pictos;");
+			stat.executeUpdate("drop table if exists android_metadata;");
+			
+			stat.executeUpdate(DBResources.CREATE_TABLE_CATEGORYS);
+			stat.executeUpdate(DBResources.CREATE_TABLE_SUBCATEGORYS);
+			stat.executeUpdate(DBResources.CREATE_TABLE_PICTOS);
+			stat.executeUpdate(DBResources.CREATE_TABLE_RECENTPICTOS);
+			stat.executeUpdate(DBResources.CREATE_TABLE_IMAGES);
+			stat.executeUpdate(DBResources.CREATE_TABLE_SUBCATEGORYS_PICTOS);
+			
+			stat.executeUpdate(DBResources.CREATE_FKI_CATEGORYS_IMAGES);
+			stat.executeUpdate(DBResources.CREATE_FKI_SUBCATEGORYS_IMAGES);
+			stat.executeUpdate(DBResources.CREATE_FKI_SUBCATEGORYS_CATEGORYS);
+			stat.executeUpdate(DBResources.CREATE_FKI_PICTOS_IMAGES);
+			stat.executeUpdate(DBResources.CREATE_FKI_RECENTPICTOS_IMAGES);
+			stat.executeUpdate(DBResources.CREATE_FKI_SUBCATEGORYS_PICTOS_PICTOID);
+			stat.executeUpdate(DBResources.CREATE_FKI_SUBCATEGORYS_PICTOS_SUBCATID);
+
+			stat.executeUpdate(DBResources.CREATE_FKU_CATEGORYS_IMAGES);
+			stat.executeUpdate(DBResources.CREATE_FKU_SUBCATEGORYS_IMAGES);
+			stat.executeUpdate(DBResources.CREATE_FKU_SUBCATEGORYS_CATEGORYS);
+			stat.executeUpdate(DBResources.CREATE_FKU_PICTOS_IMAGES);
+			stat.executeUpdate(DBResources.CREATE_FKU_RECENTPICTOS_IMAGES);
+			stat.executeUpdate(DBResources.CREATE_FKU_SUBCATEGORYS_PICTOS_PICTOID);
+			stat.executeUpdate(DBResources.CREATE_FKU_SUBCATEGORYS_PICTOS_SUBCATID);
+			
+			stat.executeUpdate(DBResources.CREATE_ANDROID_METADATA);
+			stat.executeUpdate(DBResources.INSERT_ANDROID_METADATA);
+			//Images
+			stat.executeUpdate("insert into Images (imageDesc, imageUri) values"
+					+"('Cat Food', 'Songs/FloRida/cat food.png');");
+			stat.executeUpdate("insert into categorys(categoryName,categoryDesc,imageID)" +
+					" values ('Favourites','Favourites',1)");
+			
+			conn.setAutoCommit(false); //Transaction
+			
+			File[] files = new File("/Users/c2lieu/Desktop/picto").listFiles();
+		   			
+			prep = conn.prepareStatement("insert into Categorys (categoryName, categoryDesc, imageID) values(?, ?, 1)",Statement.RETURN_GENERATED_KEYS);
+		    //Insert Categories
+		    for(File f : files){
+		    	if(f.isDirectory()){
+		    		//System.out.println(f.getName());
+		    		prep.setString(1, f.getName());
+		    		prep.setString(2, f.getName());		    		
+		    		prep.executeUpdate();
+		    		
+		    		keys = prep.getGeneratedKeys();
+
+					if(keys.next()){
+						catMap.put(f.getName(), keys.getLong(1));
+					}
+					
+					conn.commit();
+		    	}
+		    }
+		    
+		    prep = conn.prepareStatement("insert into Subcategorys (subcategoryName, subcategoryDesc, categoryID,imageID) values"
+					+"(?, ?, ?, 1);",Statement.RETURN_GENERATED_KEYS);
+		    //Insert Subcategories
+		    for(File f : files){
+		    	if(f.isDirectory()){
+		    		String cat = f.getName();
+		    		File[] subs = f.listFiles();
+		    		for(File f2 : subs){
+		    			if(f2.isDirectory()){
+		    				//System.out.println(cat + ": " + f2.getName());
+		    				prep.setString(1, f2.getName());
+		    				prep.setString(2, f2.getName());
+		    				prep.setLong(3, catMap.get(cat));
+		    				prep.executeUpdate();
+		    				
+		    				keys = prep.getGeneratedKeys();
+
+		    				if(keys.next()){
+		    					subMap.put(f2.getName(), keys.getLong(1));
+		    				}
+
+		    				conn.commit();
+		    			}
+		    		}
+		    	}
+		    }
+		    
+		    prep = conn.prepareStatement("insert into Images (imageDesc, imageUri) values"
+					+"(?, ?);",Statement.RETURN_GENERATED_KEYS);
+		    PreparedStatement prepPicto = conn.prepareStatement("insert into Pictos (pictoPhrase, imageID,playCount) values"
+					+"(?, ?,0);",Statement.RETURN_GENERATED_KEYS);
+		    PreparedStatement prepPictoSub = conn.prepareStatement("insert into Subcategorys_Pictos (subcategoryID, pictoID) values"
+					+"(?, ?);",Statement.RETURN_GENERATED_KEYS);
+		    //Insert Images, Pictos, and Subcategory_Pictos
+		    for(File f : files){
+		    	if(f.isDirectory()){
+		    		String cat = f.getName();
+		    		File[] subs = f.listFiles();
+		    		for(File f2 : subs){
+		    			if(f2.isDirectory()){
+		    				File[] pictos = f2.listFiles();
+		    				String sub = f2.getName();
+		    				for(File picto : pictos){
+		    					if(picto.getName().matches(".*\\.png$")){
+		    						prep.setString(1, picto.getName());
+		    						prep.setString(2, cat + "/" + sub + "/" + picto.getName());
+		    						prep.executeUpdate();
+		    						
+		    						keys = prep.getGeneratedKeys();
+		    						
+		    						Long imgId = (long) 0;
+		    						if(keys.next()) imgId = keys.getLong(1);
+		    						
+		    						prepPicto.setString(1, rmExt(picto.getName(),".png"));
+		    						prepPicto.setLong(2, imgId);
+		    						prepPicto.executeUpdate();
+		    						
+		    						keys = prepPicto.getGeneratedKeys();
+		    						
+		    						Long pictoID = (long) 0;
+		    						if(keys.next()) pictoID = keys.getLong(1);
+		    						
+		    						prepPictoSub.setLong(1, subMap.get(sub));
+		    						prepPictoSub.setLong(2, pictoID);
+		    						prepPictoSub.executeUpdate();
+		    						
+		    						conn.commit();
+		    						
+		    					}
+		    				}
+		    			}
+		    		}
+		    	}
+		    }
+		    
+		    
+			conn.close();
+		} catch(SQLException ex){
+			// if the error message is "out of memory", 
+			// it probably means no database file is found
+			System.err.println(ex.getMessage());
+			
+		} catch(ClassNotFoundException ex){
+			System.err.println(ex.getMessage());
+		} finally{
+			try {
+				if(conn != null)
+					conn.close();
+			} catch(SQLException e)	{
+				// connection close failed.
+				System.err.println(e);
+			}
+		}
+	}
+	
+	private static String rmExt(String file, String ext)
+	{
+		if(file.endsWith(ext))
+			return file.substring(0,file.length() - ext.length());
+		else
+			return file;
+	}
+
+}
---
+++ trunk/DBHelper/src/DBResources.java
@@ -0,0 +1,176 @@
+
+public class DBResources {
+
+	public static final String CREATE_TABLE_CATEGORYS = "CREATE TABLE Categorys"  + " ( "
+			+ "categoryID INTEGER PRIMARY KEY AUTOINCREMENT, "
+			+ "imageID INTEGER REFERENCES Images (imageID) NOT NULL, "
+			+ "categoryName TEXT, "
+			+ "categoryDesc TEXT "
+			+ ");";
+	
+	public static final String CREATE_TABLE_SUBCATEGORYS = "CREATE TABLE Subcategorys" + " ( "
+			+ "subcategoryID INTEGER PRIMARY KEY AUTOINCREMENT, "
+			+ "categoryID INTEGER REFERENCES Categorys (categoryID) NOT NULL, "
+			+ "imageID INTEGER REFERENCES Images (imageID) NOT NULL, "
+			+ "subcategoryName TEXT, "
+			+ "subcategoryDesc TEXT "
+			+ ");";
+	
+	public static final String CREATE_TABLE_PICTOS = "CREATE TABLE Pictos" + " ( "
+			+ "pictoID INTEGER PRIMARY KEY AUTOINCREMENT, "			
+			+ "imageID INTEGER REFERENCES Images (imageID) NOT NULL, "
+			+ "pictoPhrase TEXT,"
+			+ "playCount INTEGER"
+			+ ");";
+	
+	public static final String CREATE_TABLE_RECENTPICTOS = "CREATE TABLE RecentPictos" + " ( "
+			+ "recentPictoID INTEGER PRIMARY KEY AUTOINCREMENT, "			
+			+ "imageID INTEGER REFERENCES Images (imageID) NOT NULL, "
+			+ "pictoID INTEGER REFERENCES Pictos (pictoID) NOT NULL, "
+			+ "recentPictoPhrase TEXT, "
+			+ "recentPictoOutdated INTEGER "
+			+ ");";
+			
+	public static final String CREATE_TABLE_IMAGES = "CREATE TABLE Images" + " ( "
+			+ "imageID INTEGER PRIMARY KEY AUTOINCREMENT, "						
+			+ "imageDesc TEXT, "
+			+ "imageUri TEXT "
+			+ ");";
+	
+	public static final String CREATE_TABLE_SUBCATEGORYS_PICTOS = "CREATE TABLE Subcategorys_Pictos"  + " ( "
+			+ "subcategorys_PictosID INTEGER PRIMARY KEY AUTOINCREMENT, "						
+			+ "subcategoryID INTEGER REFERENCES Subcategorys (subcategoryID) NOT NULL, "
+			+ "pictoID INTEGER REFERENCES Pictos (pictoID) NOT NULL "
+			+ ");";
+
+	//FKI
+	public static final String CREATE_FKI_CATEGORYS_IMAGES = "CREATE TRIGGER fki_categorys_images " +
+			"BEFORE INSERT ON Categorys " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT imageID FROM Images WHERE imageID = NEW.imageID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'insert on table \"categorys\" violates foreign key constraint \"fki_categorys_images\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_FKI_SUBCATEGORYS_IMAGES = "CREATE TRIGGER fki_subcategorys_images " +
+			"BEFORE INSERT ON Subcategorys " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT imageID FROM Images WHERE imageID = NEW.imageID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'insert on table \"subcategorys\" violates foreign key constraint \"fki_subcategorys_images\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_FKI_SUBCATEGORYS_CATEGORYS = "CREATE TRIGGER fki_subcategorys_categorys " +
+			"BEFORE INSERT ON Subcategorys " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT categoryID FROM Categorys WHERE categoryID = NEW.categoryID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'insert on table \"subcategorys\" violates foreign key constraint \"fki_subcategorys_categorys\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_FKI_PICTOS_IMAGES = "CREATE TRIGGER fki_pictos_images " +
+			"BEFORE INSERT ON Pictos " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT imageID FROM Images WHERE imageID = NEW.imageID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'insert on table \"pictos\" violates foreign key constraint \"fki_pictos_images\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_FKI_RECENTPICTOS_IMAGES = "CREATE TRIGGER fki_recentpictos_images " +
+			"BEFORE INSERT ON RecentPictos " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT imageID FROM Images WHERE imageID = NEW.imageID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'insert on table \"RecentPictos\" violates foreign key constraint \"fki_recentpictos_images\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_FKI_SUBCATEGORYS_PICTOS_PICTOID = "CREATE TRIGGER fki_subcategorys_pictos_pictoid " +
+			"BEFORE INSERT ON SUBCATEGORYS_PICTOS " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT pictoID FROM Pictos WHERE pictoID = NEW.pictoID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'insert on table \"SUBCATEGORYS_PICTOS\" violates foreign key constraint \"fki_subcategorys_pictos_pictoid\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_FKI_SUBCATEGORYS_PICTOS_SUBCATID = "CREATE TRIGGER fki_subcategorys_pictos_subcatID " +
+			"BEFORE INSERT ON Subcategorys_Pictos " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT subcategoryID FROM Subcategorys WHERE subcategoryID = NEW.subcategoryID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'insert on table \"SUBCATEGORYS_PICTOS\" violates foreign key constraint \"fki_subcategorys_pictos_subcatID\"') " +
+			"END; "+
+			"END; ";
+	
+	//FKU
+	public static final String CREATE_FKU_CATEGORYS_IMAGES = "CREATE TRIGGER fku_categorys_images " +
+			"BEFORE UPDATE ON Categorys " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT imageID FROM Images WHERE imageID = NEW.imageID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'update on table \"categorys\" violates foreign key constraint \"fku_categorys_images\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_FKU_SUBCATEGORYS_IMAGES = "CREATE TRIGGER fku_subcategorys_images " +
+			"BEFORE UPDATE ON Subcategorys " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT imageID FROM Images WHERE imageID = NEW.imageID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'update on table \"subcategorys\" violates foreign key constraint \"fku_subcategorys_images\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_FKU_SUBCATEGORYS_CATEGORYS = "CREATE TRIGGER fku_subcategorys_categorys " +
+			"BEFORE UPDATE ON Subcategorys " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT categoryID FROM Categorys WHERE categoryID = NEW.categoryID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'update on table \"subcategorys\" violates foreign key constraint \"fku_subcategorys_categorys\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_FKU_PICTOS_IMAGES = "CREATE TRIGGER fku_pictos_images " +
+			"BEFORE UPDATE ON Pictos " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT imageID FROM Images WHERE imageID = NEW.imageID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'update on table \"pictos\" violates foreign key constraint \"fku_pictos_images\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_FKU_RECENTPICTOS_IMAGES = "CREATE TRIGGER fku_recentpictos_images " +
+			"BEFORE UPDATE ON RecentPictos " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT imageID FROM Images WHERE imageID = NEW.imageID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'update on table \"RecentPictos\" violates foreign key constraint \"fku_recentpictos_images\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_FKU_SUBCATEGORYS_PICTOS_PICTOID = "CREATE TRIGGER fku_subcategorys_pictos_pictoid " +
+			"BEFORE UPDATE ON SUBCATEGORYS_PICTOS " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT pictoID FROM Pictos WHERE pictoID = NEW.pictoID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'update on table \"SUBCATEGORYS_PICTOS\" violates foreign key constraint \"fku_subcategorys_pictos_pictoid\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_FKU_SUBCATEGORYS_PICTOS_SUBCATID = "CREATE TRIGGER fku_subcategorys_pictos_subcatID " +
+			"BEFORE UPDATE ON Subcategorys_Pictos " +
+			"FOR EACH ROW BEGIN " +
+			"	SELECT CASE " +
+			"	WHEN ( (SELECT subcategoryID FROM Subcategorys WHERE subcategoryID = NEW.subcategoryID) IS NULL) " +
+			"	THEN RAISE(ABORT, 'update on table \"SUBCATEGORYS_PICTOS\" violates foreign key constraint \"fku_subcategorys_pictos_subcatID\"') " +
+			"END; "+
+			"END; ";
+	
+	public static final String CREATE_ANDROID_METADATA = "CREATE TABLE \"android_metadata\" (\"locale\" TEXT DEFAULT 'en_US')";
+	public static final String INSERT_ANDROID_METADATA = "INSERT INTO \"android_metadata\" VALUES ('en_US')";
+}
