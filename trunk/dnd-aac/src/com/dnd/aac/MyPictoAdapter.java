90c3561289cf321effcffa23830dffbb61c6958e
davidly16@gmail.com <davidly16@gmail.com@2b09df3e-bf32-aa2d-f863-fad657680e6d>
2012-06-05 20:14:59 +0000

    Added Disk/Memory Cache.

--- trunk/dnd-aac/src/com/dnd/aac/MyPictoAdapter.java
+++ trunk/dnd-aac/src/com/dnd/aac/MyPictoAdapter.java
@@ -11,36 +11,41 @@ import android.graphics.BitmapFactory;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.android.vending.expansion.zipfile.APKExpansionSupport;
-import com.android.vending.expansion.zipfile.ZipResourceFile;
+import com.dnd.aac.cache.DiskLruCache;
+import com.dnd.aac.cache.ImageResizer;
 
 public class MyPictoAdapter extends BaseAdapter{
 	  private Context mContext;
 	  private int layout;
 	  private Cursor mCursor;
-	  private ZipResourceFile mExpansionFile;
 	  private int uriIndex;
 	  private int phraseIndex;
 	  
-	    public MyPictoAdapter(Context c,int layout,Cursor cursor) {
-	        mContext = c;
-	        this.layout = layout;
-	        mCursor = cursor;
-	        uriIndex = mCursor.getColumnIndex("imageUri");
-	        phraseIndex = mCursor.getColumnIndex("pictoPhrase");
-	        try {
-				mExpansionFile = APKExpansionSupport.getAPKExpansionZipFile(mContext,
-				        1, 0);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			};
-	    }
+	  private int mItemHeight = 0;
+      private int mNumColumns = 0;
+      private int mActionBarHeight = -1;
+      private GridView.LayoutParams mImageViewLayoutParams;
+      private ImageResizer mImageWorker;
+      
+      
+	  
+	  private int uriIndex;
+	  private int phraseIndex;
+	  
+	    public MyPictoAdapter(Context c,int layout,Cursor cursor, ImageResizer mImageWorker) {
+	    	 mContext = c;
+		        this.layout = layout;
+		        mCursor = cursor;
+		        uriIndex = mCursor.getColumnIndex("imageUri");
+		        phraseIndex = mCursor.getColumnIndex("pictoPhrase");	
+		        this.mImageWorker = mImageWorker;
+		    }
 
 	    public int getCount() {
 	        return mCursor.getCount();
@@ -52,38 +57,62 @@ public class MyPictoAdapter extends BaseAdapter{
 
 	    public long getItemId(int position) {
 	        mCursor.moveToPosition(position);
-	        return mCursor.getLong(mCursor.getColumnIndexOrThrow("_id"));
+	        if (mCursor.getColumnIndex("_id") == -1) return 0;
+	        return mCursor.getLong(mCursor.getColumnIndex("_id"));
 	    }
+	    
+	    public void setNumColumns(int numColumns) {
+            mNumColumns = numColumns;
+        }
+
+        public int getNumColumns() {
+            return mNumColumns;
+        }
+        
+        public void setItemHeight(int height) {
+            if (height == mItemHeight) {
+                return;
+            }
+            mItemHeight = height;
+            mImageViewLayoutParams =
+                    new GridView.LayoutParams(LayoutParams.MATCH_PARENT, mItemHeight);
+            mImageWorker.setImageSize(height);
+            notifyDataSetChanged();
+        }
 
 	    // create a new ImageView for each item referenced by the Adapter
-	    @Override
-		public View getView(int position, View convertView, ViewGroup parent) {
+	    public View getView(int position, View convertView, ViewGroup parent) {
 	        mCursor.moveToPosition(position);
 	        ViewHolder holder;
+	        	        
 	        if (convertView == null) {  
 	        	convertView = ( (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) ).inflate(layout,null);
 	        	convertView.setLayoutParams(new GridView.LayoutParams(115,115));
-	        	
+	        		        	
 	        	holder = new ViewHolder();
 	        	holder.text = (TextView)convertView.findViewById(R.id.text);
 	        	holder.image = (ImageView)convertView.findViewById(R.id.image);
+	        	holder.image.setScaleType(ImageView.ScaleType.CENTER_CROP);
 	        	convertView.setTag(holder);
 	        } else {
 	        	holder = (ViewHolder) convertView.getTag();
 	        }
 	        	
 	        holder.text.setText(mCursor.getString(phraseIndex));	        	
-	        	
-	        try {
-	        	InputStream fileStream = mExpansionFile.getInputStream("picto/" + mCursor.getString(mCursor.getColumnIndex("imageUri")));
-//	        	BufferedInputStream buf = new BufferedInputStream(new FileInputStream("/mnt/sdcard/Android/data/com.dnd.aac/main.1.com.dnd.aac/picto/" + mCursor.getString(uriIndex)));
-	        	BufferedInputStream buf = new BufferedInputStream(fileStream);
-	        	Bitmap bitmap = BitmapFactory.decodeStream(buf);
-	        	holder.image.setImageBitmap(bitmap);
-	        	buf.close();
-	        } catch (IOException e) {
-	        	e.printStackTrace();
-	        }
+	        
+//	        try {
+//	        	InputStream fileStream = MainActivity.mExpansionFile.getInputStream("picto/" + mCursor.getString(mCursor.getColumnIndex("imageUri")));
+//	        	
+////	        	BufferedInputStream buf = new BufferedInputStream(new FileInputStream("/mnt/sdcard/Android/data/com.dnd.aac/main.1.com.dnd.aac/picto/" + mCursor.getString(uriIndex)));
+//	        	BufferedInputStream buf = new BufferedInputStream(fileStream);
+//	        	Bitmap bitmap = BitmapFactory.decodeStream(buf);
+//	        	holder.image.setImageBitmap(bitmap);
+//	        	buf.close();
+//	        } catch (IOException e) {
+//	        	e.printStackTrace();
+//	        }
+	        
+	        mImageWorker.loadImage(mCursor.getString(mCursor.getColumnIndex("imageUri")), holder.image);
 		        
 	        return convertView;
 	    }
@@ -91,7 +120,8 @@ public class MyPictoAdapter extends BaseAdapter{
 		class ViewHolder{
 			public TextView text;
 			public ImageView image;
-		}
+		}		
+		
 }
 
 
